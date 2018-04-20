127f857e8d5ddef07304531a493b41183bb620f1
YotamWolf <38569619+YotamWolf@users.noreply.github.com>
2018-04-20 21:45:16 +0300

    Add files via upload

--- FilesToCell.m
+++ FilesToCell.m
@@ -5,9 +5,36 @@ function [Cells] = FilesToCell
     for k = 1:length(myFiles)
       baseFileName = myFiles(k).name;
       NameArguments = strsplit(baseFileName, '_');
-      
       Cells{k,1} = NameArguments{2};
-      Cells{k,2} = NameArguments{end};
+      if length(NameArguments)>5
+          Cells{k,1} =strcat(NameArguments{2},'_',NameArguments{3});
+      end
+      filetype = strsplit(NameArguments{end}, '.');
+      Cells{k,2} = filetype(1);
       fullFileName = fullfile(myFiles(k).folder, baseFileName);
       Cells{k,3} = xlsread(fullFileName);
-    end
+      switch Cells{k,1}
+          case 'fw_fall'
+              Cells{k,1}=1;
+          case 'sd_fall'
+              Cells{k,1}=2;
+          case 'bk_fall'
+              Cells{k,1}=3;
+          case 'walking'
+              Cells{k,1}=4;
+          case 'Sitting'
+              Cells{k,1}=5;
+          case 'sit_typ'
+              Cells{k,1}=6;
+          case 'dn_strs'
+              Cells{k,1}=7;
+          case 'up_strs'
+              Cells{k,1}=8;
+          case 'sit_car'
+              Cells{k,1}=9;
+          case 'wlk_off'
+              Cells{k,1}=10;
+      end           
+    end  
+end
+    
